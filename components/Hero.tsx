3232a543432feb17130795beade91163f57644ac
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-11 22:04:42 +0700

    update-FixStyles-HeroSections

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,8 +13,14 @@ const Hero = () => {
                         Find, book, rent a car—quick and super easy!
                     </h1>
 
-                    <p className='hero__subtitle'>
-                        Streamline your car rental experience with our effortless booking
+                    <p
+                        className='hero__subtitle border-2
+                        p-4 flex items-center justify-center rounded-md
+                        hover:bg-gray-100 transition-all
+                        duration-200 ease-in-out'
+                    >
+                        Streamline your car rental experience
+                        with our effortless booking
                         process.
                     </p>
 
@@ -32,7 +38,7 @@ const Hero = () => {
                         />
                     </div>
 
-                    <div className='hero__image-overplay' />
+                    <div className='hero__image-overlay' />
                 </div>
             </div>
         </>
