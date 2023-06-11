eb5a0019f9da89bcc9264d522ea869cb1355c189
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-11 10:16:33 +0700

    update-AddHero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import { Hero } from '@/components';
 
 export default function Home() {
   return (
-    <main>
-
+    <main className='overflow-hidden'>
+      <Hero />
     </main>
-  )
-}
+  );
+};
