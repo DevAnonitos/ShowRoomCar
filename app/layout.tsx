75629e8ffec6a38219aece835db61ae8774c3ab1
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-12 20:45:49 +0700

    update-Add-Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className='relative'>
         <Navbar />
-        {children}
+          {children}
         <Footer />
       </body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,97 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import CustomButton from './CustomButton';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <>
+            <header
+                className='w-full absolute z-10'
+            >
+                <nav
+                    className='max-w-[1440px] mx-auto flex justify-between
+                    items-center sm:px-6 py-4 bg-transparent'
+                >
+                    <Link
+                        href="/"
+                        className='flex justify-center items-center'
+                    >
+                        <Image
+                            src="/logo.svg"
+                            alt='logo'
+                            width={118}
+                            height={18}
+                            className='object-contain'
+                        />
+                    </Link>
+
+                    <header className="text-gray-600 body-font">
+                        <div
+                            className="container mx-auto flex flex-wrap
+                            p-5 flex-col md:flex-row items-center"
+                        >
+
+                            <nav
+                                className="md:ml-auto flex flex-wrap
+                                items-center text-base justify-center"
+                            >
+                                <a
+                                    className="mr-5 hover:text-gray-900
+                                    border-2 py-1 px-3 rounded-full border-gray-500"
+                                >
+                                    Home
+                                </a>
+                                <a
+                                    className="mr-5 hover:text-gray-900
+                                    border-2 py-1 px-3 rounded-full border-gray-500"
+                                >
+                                    About
+                                </a>
+                                <a
+                                    className="mr-5 hover:text-gray-900
+                                    border-2 py-1 px-3 rounded-full border-gray-500"
+                                >
+                                    Contact
+                                </a>
+                                <a
+                                    className="mr-5 hover:text-gray-900 border-2
+                                    py-1 px-3 rounded-full border-gray-500"
+                                >
+                                    View
+                                </a>
+                            </nav>
+                            <button
+                                className="inline-flex items-center
+                                bg-gray-100 border-0 py-1 px-3
+                                focus:outline-none hover:bg-gray-200
+                                rounded-full text-base mt-4 md:mt-0">
+                                    Visit
+                                <svg
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    className="w-4 h-4 ml-1"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </header>
+
+                    <CustomButton
+                        title='SignIn'
+                        btnType='button'
+                        containerStyles='text-primary-blue
+                        rounded-full bg-white min-w-[130px]'
+                    />
+                </nav>
+            </header>
+        </>
     );
 };
 
