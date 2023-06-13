b81ebd7b61abe835b3e6e76080e21895e77c00bb
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-13 23:26:43 +0700

    update-AddCar-Catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import Image from 'next/image';
-import { Hero } from '@/components';
+import { Hero, SearchBar, CarCard, ShowMore, CustomFilter } from '@/components';
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+
+      <div
+        className='mt-12 padding-x padding-y max-width'
+        id='discover'
+      >
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+          <p>
+            Explore out cars you might like
+          </p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filters-container'>
+            <CustomFilter />
+            <CustomFilter />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
