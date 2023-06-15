c2bc6744a2f953ec536ded1adf38ff9baf191074
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-15 21:28:07 +0700

    update-Add-CarProps

--- types/index.ts
+++ types/index.ts
@@ -9,6 +9,20 @@ export interface FilterProps {
     fuel?: string;
 }
 
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
 
 export interface CustomButtonProps {
     isDisabled?: boolean;
