271676becf8ae6c8e036bfaaa7a2d21dd8cd1ebc
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-15 15:40:16 +0700

    update-add-SearchManufacturer-Items

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -76,8 +76,45 @@ const SearchManufacturer = (
                                         </Combobox.Option>
                                     </>
                                 ): (
-                                    <>
-                                    </>
+                                    filteredManufacturers.map((item) => (
+                                        <Combobox.Option
+                                            key={item}
+                                            className={({ active }) =>
+                                                `relative search-manufacturer__option ${
+                                                    active ? "bg-primary-blue text-white" : "text-gray-900"
+                                                }`
+                                            }
+                                            value={item}
+                                        >
+                                            {({selected, active}) => (
+                                                <>
+                                                    <span
+                                                        className={
+                                                            `block truncate
+                                                            ${selected
+                                                            ? "font-medium"
+                                                            : "font-normal"}`
+                                                        }
+                                                    >
+                                                        {item}
+                                                    </span>
+
+                                                    {selected ? (
+                                                        <>
+                                                            <span
+                                                                className={`
+                                                                    absolute inset-y-0 left-0
+                                                                    flex items-center pl-3
+                                                                    ${active? "text-white"
+                                                                    : "text-pribg-primary-purple"}`
+                                                                }
+                                                            ></span>
+                                                        </>
+                                                    ): null}
+                                                </>
+                                            )}
+                                        </Combobox.Option>
+                                    ))
                                 )}
                             </Combobox.Options>
                         </Transition>
