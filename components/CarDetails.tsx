4988e787e51bdd8fe351a48d29d2fc728be7343c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-21 22:28:00 +0700

    update-AddCarDetails

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -78,7 +78,63 @@ const CarDetails = ({ isOpen = false, closeModal, car }: CarDetailsProps) => {
                                             className='relative w-full h-40
                                             bg-pattern bg-cover rounded-lg'
                                         >
+                                            <div
+                                                className='relative w-full h-40
+                                                bg-pattern bg-cover bg-center rounded-lg'
+                                            >
+                                                <Image
+                                                    src={generateCarImageUrl(car)}
+                                                    alt='car model'
+                                                    fill
+                                                    priority
+                                                    className='object-contain'
+                                                />
+                                            </div>
 
+                                            <div className='flex gap-3'>
+                                                <div
+                                                    className='flex-1 relative w-full h-24
+                                                    bg-primary-blue-100 rounded-lg'
+                                                >
+                                                    <Image
+                                                        src={generateCarImageUrl(car, "29")}
+                                                        alt='car model'
+                                                        fill
+                                                        priority
+                                                        className='object-contain'
+                                                    />
+                                                </div>
+                                                <div
+                                                    className='flex-1 relative w-full h-24
+                                                    bg-primary-blue-100 rounded-lg'
+                                                >
+                                                    <Image
+                                                        src={generateCarImageUrl(car, "33")}
+                                                        alt='car model'
+                                                        fill
+                                                        priority
+                                                        className='object-contain'
+                                                    />
+                                                </div>
+                                                <div
+                                                    className='flex-1 relative w-full h-24
+                                                    bg-primary-blue-100 rounded-lg'
+                                                >
+                                                    <Image
+                                                        src={generateCarImageUrl(car, "13")}
+                                                        alt='car model'
+                                                        fill
+                                                        priority
+                                                        className='object-contain'
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className='flex-1 flex flex-col gap-2 z-50'>
+                                            <h2 className='font-semibold text-xl capitalize'>
+                                                {car.make} {car.model}
+                                            </h2>
                                         </div>
                                     </div>
                                 </Dialog.Panel>
--- utils/index.ts
+++ utils/index.ts
@@ -63,4 +63,9 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
 };
